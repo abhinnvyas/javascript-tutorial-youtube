3314d8dfb0e38eab1af8d76b72ba564f73f35a3a
Abhinn Vyas <abhinnvyas2@gmail.com>
2021-05-26 14:02:52 -0700

    More files

---
+++ condtionals.js
@@ -0,0 +1,11 @@
+var isMilkAvailable = true; //bolean
+
+// if (isMilkAvailable) {
+//     console.log("Get the milk.")
+// } else {
+//     console.log("Get the milk powder")
+// }
+
+// (condtion) ? (truecode) : (falsecode)
+
+(isMilkAvailable) ? console.log("Get the milk") : console.log("Get the milk powder")
---
+++ variables.js
@@ -0,0 +1,19 @@
+// var let const
+
+var name1 = "Abhinn Vyas";
+
+var name2;
+name2 = "Abhinn2";
+
+// console.log("the value of name1 is", name1)
+// console.log("the value of name2 is", name2)
+
+console.log(`the value of name1 is ${name1}
+the value of name2 is ${name2}`)
+
+
+// undefined
+// null
+// NaN
+// Boolean
+
