5c92438e402bca81aa2e5d32aea61e4169da8c43
Abhinn Vyas <abhinnvyas2@gmail.com>
2021-05-31 11:50:29 -0700

    episode6&7

---
+++ functions.js
@@ -0,0 +1,17 @@
+// var channelName = "Abhinn Vyas";
+
+// function saySubscribe(channel) {
+//     console.log("Subscribe Now to", channel);
+// }
+
+// saySubscribe(channelName);
+
+function add(a, b) {
+    // console.log(a + b);
+    return a + b;
+}
+
+var result = add(5, 5);
+// console.log(result)
+
+add;
